13143abcaab30c798e0d6355e5564b490ab069d3
E. M. Greeff <unmodster@gmail.com>
2021-10-17 20:40:17 -0600

    Update AnonymousFunctionsExample.lua

--- AnonymousFunctionsExample.lua
+++ AnonymousFunctionsExample.lua
@@ -4,3 +4,8 @@ delay(2, function(exactTimeElapsed)
 end)
 
 -- ExaMPLE2
+local Players = game:GetService("Players")
+ 
+Players.PlayerAdded:Connect(function(player)
+	print(player.Name .. " joined the game!")
+end)
